6b119759a62b036bc6f2cecc8c5bc4b7dd6565e1
mvkozak-fe <mvkozak.fe@gmail.com>
2023-12-25 02:57:25 +0300

    feat:add root function to bts task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,16 +6,44 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 class BinarySearchTree {
-
+  bst = null;
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.bst;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const node = new Node(data);
+    if (!this.bst) {
+      this.bst = node;
+      return;
+      // return this.bst;
+    }
+    let cur = this.bst;
+    while (cur) {
+      if (data < cur.data) {
+        if (cur.left) {
+          cur = cur.left
+        } else {
+          cur.left = node;
+          return ;
+        }
+      } else {
+        if (cur.rigth) {
+          cur = cur.right;
+        } else {
+          cur.rigth = node;
+          return ;
+        }
+      }
+    }
   }
 
   has(/* data */) {
@@ -26,6 +54,25 @@ class BinarySearchTree {
   find(/* data */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    // let cur = this.bst;
+    // if (!cur || !data) {
+    //   return null;
+    // }
+    // while (cur) {
+    //   if (cur.data == data) {
+    //     return cur;
+    //   } 
+    //   if (data < cur.data) {
+    //     cur = cur.left;
+    //     return cur;
+       
+    //   } else {
+    //     cur = cur.right;
+    //     return cur;        
+    //   }
+      
+    // } return null;
+    
   }
 
   remove(/* data */) {
@@ -34,13 +81,23 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    let cur = this.bst;
+    while (cur.left) {
+      cur = cur.left;
+    }
+
+    return cur.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+    let cur = this.bst;
+    while (cur.right) {
+      cur = cur.right;
+    }
+
+    return cur.data;;
   }
 }
 
