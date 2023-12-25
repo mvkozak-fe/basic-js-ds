736309feefef6d885c344a6d1a7130a3ccf79712
mvkozak-fe <mvkozak.fe@gmail.com>
2023-12-25 16:57:06 +0300

    feat: add bst task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,103 +1,130 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
-  }
-}
+
 class BinarySearchTree {
-  bst = null;
+  constructor() {
+    this.tree = null;
+  }
+  
   root() {
-    return this.bst;
+    return this.tree;
   }
 
   add(data) {
-    const node = new Node(data);
-    if (!this.bst) {
-      this.bst = node;
-      return;
-      // return this.bst;
-    }
-    let cur = this.bst;
-    while (cur) {
-      if (data < cur.data) {
-        if (cur.left) {
-          cur = cur.left
-        } else {
-          cur.left = node;
-          return ;
-        }
+    this.tree = addNode(this.tree, data);
+
+    function addNode(node, data) {
+      if (!node) {
+        return new Node(data);
+      }
+
+      if (node.data === data) {
+        return node;
+      }
+
+      if (data < node.data) {
+        node.left = addNode(node.left, data);
       } else {
-        if (cur.rigth) {
-          cur = cur.right;
-        } else {
-          cur.rigth = node;
-          return ;
-        }
+        node.right = addNode(node.right, data);
+      }
+
+      return node;
+    }    
+  }
+  has(data) {
+    return searchNode(this.tree, data)
+    function searchNode(node, data) {
+      if (!node) {
+        return false;
+      }
+      if (node.data === data) {
+        return true
+      }
+      if (data < node.data) {
+        return searchNode(node.left, data)
+      } else {
+        return searchNode(node.right, data)
       }
     }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-    // let cur = this.bst;
-    // if (!cur || !data) {
-    //   return null;
-    // }
-    // while (cur) {
-    //   if (cur.data == data) {
-    //     return cur;
-    //   } 
-    //   if (data < cur.data) {
-    //     cur = cur.left;
-    //     return cur;
-       
-    //   } else {
-    //     cur = cur.right;
-    //     return cur;        
-    //   }
-      
-    // } return null;
+  find(data) {
+    return searchNodeData(this.tree, data)
+    function searchNodeData(node, data) {
+      if (!node) {
+        return null;
+      }
+      if (node.data === data) {
+        return node;
+      }
+      if (data < node.data) {
+        return searchNodeData(node.left, data)
+      } else {
+        return searchNodeData(node.right, data)
+      }
+    }
     
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.tree = removeNode(this.tree, data);
+    function removeNode(node, data) {
+      if (!node) {
+        return null;
+      }
+      if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node; 
+      } else if (data > node.data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null;
+        }
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+        let minFromRight = node.right;
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        } 
+        node.data = minFromRight.data;
+        node.right = removeNode(node.right, minFromRight.data);
+        return node;
+        
+      }
+
+    }
+
   }
 
   min() {
-
-    let cur = this.bst;
-    while (cur.left) {
-      cur = cur.left;
-    }
-
-    return cur.data;
+    let node = this.tree;
+    while (node.left) {
+      node = node.left;
+    } 
+    return node.data;
+    
   }
 
   max() {
-  
-    let cur = this.bst;
-    while (cur.right) {
-      cur = cur.right;
-    }
-
-    return cur.data;;
+    let node = this.tree;
+    while (node.right) {
+      node = node.right;
+    } 
+    return node.data;
   }
 }
 
